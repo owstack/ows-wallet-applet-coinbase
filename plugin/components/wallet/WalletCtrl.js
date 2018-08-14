8fd2c131314e1ca195f212e984a6acad5a62a98b
ajp8164 <andyphillipson@gmail.com>
2018-08-14 14:04:23 -0400

    Implement presentation of monitored, pending transactions in the wallet

--- plugin/components/wallet/WalletCtrl.js
+++ plugin/components/wallet/WalletCtrl.js
@@ -1,11 +1,26 @@
 'use strict';
 
-angular.module('owsWalletPlugin.controllers').controller('WalletCtrl', function($scope,
+angular.module('owsWalletPlugin.controllers').controller('WalletCtrl', function($scope, coinbaseService, popupService,
   /* @namespace owsWalletPluginClient.api */ Session) {
+
+  var coinbase = coinbaseService.coinbase;
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
     var wallet = Session.getInstance().getWalletById(data.stateParams.walletId).then(function(wallet) {
 	    $scope.wallet = wallet;
+
+	    return coinbase.getPendingTransactions(wallet.id);
+
+    }).then(function(pendingTxs) {
+
+    	// All pending transactions are formatted as Coinbase transactions.
+    	$scope.pendingTxs = pendingTxs;
+
+    }).catch(function(error) {
+      popupService.showAlert(
+        gettextCatalog.getString('Uh oh!'),
+        gettextCatalog.getString('Could not get wallet information. Please try again.')
+      );
     });
   });
 
